78469e12caa1be1c25d31c8ca801665b59ca51e2
537931 <537931@gmail.com>
2024-09-08 21:47:26 -0400

    The image is shown on the full screen, and the show can be paused with space button.

--- src/slideshow/SlideShow.java
+++ src/slideshow/SlideShow.java
@@ -2,16 +2,18 @@ package slideshow;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -24,6 +26,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingWorker;
+import javax.swing.WindowConstants;
 
 public class SlideShow extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -32,13 +35,41 @@ public class SlideShow extends JFrame {
 	private Random rand = new Random();
 	private JLabel imageArea;
 	private Image currentImage;
+	private boolean paused;
 
 	public SlideShow() {
 		super( "Random slide show" );
+		setUndecorated( true );
+		setExtendedState( JFrame.MAXIMIZED_BOTH );
+		addKeyListener( new KeyListener() {
+			@Override
+			public void keyTyped( KeyEvent e ) {
+			}
+
+			@Override
+			public void keyPressed( KeyEvent e ) {
+			}
+
+			@Override
+			public void keyReleased( KeyEvent e ) {
+				if( e.getKeyCode() == KeyEvent.VK_ESCAPE ) {
+					System.exit( 0 );
+			    }
+				else if( e.getKeyCode() == KeyEvent.VK_SPACE ) {
+					paused = ! paused;
+					
+					if( paused )
+						imageArea.setBorder( BorderFactory.createLineBorder( Color.red, 4 ));
+					else
+						imageArea.setBorder( null );
+
+				}
+			}
+		});
+
 		( imageArea = new JLabel()).setLayout( new GridBagLayout());
 		imageArea.setHorizontalAlignment( JLabel.CENTER );
 		imageArea.setVerticalAlignment( JLabel.CENTER );
-		//imageArea.setBorder(BorderFactory.createLineBorder( Color.red, 4 ));
 		imageArea.setOpaque( true );
 		imageArea.setBackground( Color.black );
 		imageArea.addComponentListener( new ComponentListener() {
@@ -60,8 +91,10 @@ public class SlideShow extends JFrame {
 			public void componentHidden(ComponentEvent e) {
 			}
 		});
+
 		getContentPane().add( imageArea, BorderLayout.CENTER );
 		setSize( 500, 400 );
+		setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
 
 		addWindowListener( new WindowAdapter() {
 			public void windowClosing( WindowEvent e ) {
@@ -76,12 +109,14 @@ public class SlideShow extends JFrame {
 			protected Void doInBackground() throws Exception {
 				for( ;; ) {
 					try {
-						BufferedImage img = fetchRandomFile( path );
+						if( ! paused ) {
+							BufferedImage img = fetchRandomFile( path );
 
-						if( img == null )
-							continue;
+							if( img != null )
+								imgQueue.put( img );
+						}
 
-						imgQueue.put( img );
+						Thread.sleep( 5000 );
 					}
 					catch( Exception e ) {
 						// TODO Auto-generated catch block
@@ -96,9 +131,11 @@ public class SlideShow extends JFrame {
 			protected Void doInBackground() throws Exception {
 				for( ;; ) {
 					try {
-						currentImage = imgQueue.take();
-						System.out.println( currentImage.toString());
-						updateView();
+						if( ! paused ) {
+							currentImage = imgQueue.take();
+							updateView();
+						}
+
 						Thread.sleep( 5000 );
 					}
 					catch( Exception e ) {
@@ -115,33 +152,39 @@ public class SlideShow extends JFrame {
 
 		Image img = currentImage;
 
-		if( imageArea.getWidth() < img.getWidth( null ) ||
-			imageArea.getHeight() < img.getHeight( null ) ) {
-			double wScale = (double) img.getWidth( null ) / imageArea.getWidth();
-			double hScale = (double) img.getHeight( null ) / imageArea.getHeight();
-			double scale = Math.max( wScale, hScale );
-			int w = (int) ( img.getWidth( null ) / scale );
-			int h = (int) ( img.getHeight( null ) / scale );
-			img = img.getScaledInstance( w, h, Image.SCALE_SMOOTH );
-		}
+		double wScale = (double) img.getWidth( null ) / imageArea.getWidth();
+		double hScale = (double) img.getHeight( null ) / imageArea.getHeight();
+		double scale = Math.max( wScale, hScale );
+		int w = (int) ( img.getWidth( null ) / scale );
+		int h = (int) ( img.getHeight( null ) / scale );
+		img = img.getScaledInstance( w, h, Image.SCALE_SMOOTH );
 
 		imageArea.setIcon( new ImageIcon( img ));
-		//img = null;
-		//System.gc();
-		//imageArea.setToolTipText( img.toString());
 	}
 
 	private BufferedImage fetchRandomFile( File path ) {
 		ArrayList<File> fl = new ArrayList<File>( Arrays.asList( path.listFiles()));
-
+		
 		while( 0 < fl.size()) {
 			int i = rand.nextInt( fl.size());
 			File f = fl.get( i );
 
-			if( f.isDirectory())
-				return fetchRandomFile( f );
-			
 			try {
+				if( f.isDirectory())
+					return fetchRandomFile( f );
+
+				if( f.isFile() && f.getName().toLowerCase().endsWith( ".lnk" )) {
+					fl.remove( i );
+					LnkParser lp = new LnkParser( f );
+
+					if( lp.isDirectory()) {
+						File nf = new File( lp.getRealFilename());
+						fl.add( nf );
+						return fetchRandomFile( nf );
+					}
+				}
+
+				System.out.println( f.getCanonicalPath());
 				return ImageIO.read( f );
 			}
 			catch( IOException e ) {
@@ -154,7 +197,7 @@ public class SlideShow extends JFrame {
 
 	public static void main( String[] args ) {
 		SlideShow ss = new SlideShow();
-		ss.start( new File( "F:\\Posters" ));
+		ss.start( new File( "F:\\Screen Saver" ));
 		ss.setVisible( true );
 	}
 }
