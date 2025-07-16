72a8b176bbbd452958edeefbb06742c6821aeb91
537931 <537931@gmail.com>
2025-07-16 17:59:42 -0400

    When the show is paused the file path is displayed at the bottom of the screen.

--- src/slideshow/SlideShow.java
+++ src/slideshow/SlideShow.java
@@ -3,6 +3,7 @@ package slideshow;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Point;
@@ -28,16 +29,27 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingWorker;
 import javax.swing.WindowConstants;
+import javax.swing.border.TitledBorder;
 
 public class SlideShow extends JFrame {
 	private static final long serialVersionUID = 1L;
 
-	private BlockingQueue<Image> imgQueue = new ArrayBlockingQueue<Image>( 3 ); 
+	private BlockingQueue<Slide> imgQueue = new ArrayBlockingQueue<Slide>( 3 ); 
 	private Random rand = new Random();
 	private JLabel imageArea;
-	private Image currentImage;
+	private Slide currentSlide;
 	private Cursor savedCursor;
+	private long exposure = 7000;
 	private boolean paused;
+
+	/*
+	 * This class is introduced to couple image with its path.
+	 * When the show is paused the path is displayed.
+	 */
+	class Slide {
+		Image  image;
+		String path;
+	};
 
 	public SlideShow() {
 		super( "Random slide show" );
@@ -55,14 +67,17 @@ public class SlideShow extends JFrame {
 			@Override
 			public void keyReleased( KeyEvent e ) {
 				if( e.getKeyCode() == KeyEvent.VK_ESCAPE ) {
-					getContentPane().setCursor( savedCursor );
-					System.exit( 0 );
-			    }
+					stop();
+				}
 				else if( e.getKeyCode() == KeyEvent.VK_SPACE ) {
 					paused = ! paused;
 					
-					if( paused )
-						imageArea.setBorder( BorderFactory.createLineBorder( Color.red, 4 ));
+					if( paused ) {
+						imageArea.setBorder( BorderFactory.createTitledBorder(
+								BorderFactory.createLineBorder( Color.white, 3 ),
+								"  " + currentSlide.path + "  ", TitledBorder.CENTER, TitledBorder.BOTTOM,
+								new Font( "Monospaced", Font.BOLD, 32 ), Color.white ));
+					}
 					else
 						imageArea.setBorder( null );
 
@@ -70,16 +85,15 @@ public class SlideShow extends JFrame {
 			}
 		});
 
-		( imageArea = new JLabel()).setLayout( new GridBagLayout());
+		imageArea = new JLabel();
+		imageArea.setLayout( new GridBagLayout());
 		imageArea.setHorizontalAlignment( JLabel.CENTER );
 		imageArea.setVerticalAlignment( JLabel.CENTER );
 		imageArea.setOpaque( true );
 		imageArea.setBackground( Color.black );
 		imageArea.addComponentListener( new ComponentListener() {
-
 			@Override
 			public void componentResized(ComponentEvent e) {
-				updateView();
 			}
 
 			@Override
@@ -96,7 +110,10 @@ public class SlideShow extends JFrame {
 		});
 
 		getContentPane().add( imageArea, BorderLayout.CENTER );
-		setSize( 500, 400 );
+
+		/*
+		 * Hide the cursor
+		 */
 		savedCursor = getContentPane().getCursor();
 		getContentPane().setCursor( getContentPane().getToolkit().createCustomCursor(
                 new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB ),
@@ -105,12 +122,20 @@ public class SlideShow extends JFrame {
 
 		addWindowListener( new WindowAdapter() {
 			public void windowClosing( WindowEvent e ) {
-				getContentPane().setCursor( savedCursor );
-				System.exit( 0 );
+				stop();
 			}
 		});
 	}
 
+	/**
+	 * The method starts the show. It launches two workers.
+	 * One fetches random images walking down the path and
+	 * puts them to a small queue. The other worker takes
+	 * the images from the queue and displays them on the
+	 * screen.
+	 *
+	 * @param path images location in the file system.
+	 */
 	public void start( File path ) {
 		new SwingWorker<Void, Void>() {
 			@Override
@@ -118,17 +143,15 @@ public class SlideShow extends JFrame {
 				for( ;; ) {
 					try {
 						if( ! paused ) {
-							BufferedImage img = fetchRandomFile( path );
+							Slide s = fetchRandomFile( path );
 
-							if( img != null )
-								imgQueue.put( img );
+							if( s != null )
+								imgQueue.put( s );
 						}
 
-						Thread.sleep( 5000 );
+						Thread.sleep( exposure );
 					}
-					catch( Exception e ) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					catch( InterruptedException ignore ) {
 					}
 				}
 			}
@@ -140,26 +163,39 @@ public class SlideShow extends JFrame {
 				for( ;; ) {
 					try {
 						if( ! paused ) {
-							currentImage = imgQueue.take();
-							updateView();
+							updateView( imgQueue.take());
 						}
 
-						Thread.sleep( 5000 );
+						Thread.sleep( exposure );
 					}
-					catch( Exception e ) {
-						e.printStackTrace();
+					catch( InterruptedException ignore ) {
 					}
 				}
 			}
 		}.execute();
 	}
 
-	private void updateView() {
-		if( null == currentImage )
+	/**
+	 * The method stops the show. It restores the cursor saved
+	 * on startup and exits the application.
+	 */
+	public void stop() {
+		getContentPane().setCursor( savedCursor );
+		System.exit( 0 );
+	}
+
+	/**
+	 * The method puts a new image to the screen.
+	 */
+	private void updateView( Slide s ) {
+		if( null == s )
 			return;
 
-		Image img = currentImage;
+		Image img = s.image;
 
+		/*
+		 * Scale the image to fit the screen.
+		 */
 		double wScale = (double) img.getWidth( null ) / imageArea.getWidth();
 		double hScale = (double) img.getHeight( null ) / imageArea.getHeight();
 		double scale = Math.max( wScale, hScale );
@@ -168,9 +204,23 @@ public class SlideShow extends JFrame {
 		img = img.getScaledInstance( w, h, Image.SCALE_SMOOTH );
 
 		imageArea.setIcon( new ImageIcon( img ));
+
+		/*
+		 * Update the current slide variable so image path
+		 * can be printed when the view is paused.
+		 */
+		currentSlide = s;
 	}
 
-	private BufferedImage fetchRandomFile( File path ) {
+	/**
+	 * The method does a random walking down the file system from the point
+	 * provided as path argument. 
+	 * 
+	 * @param path starting point for the search
+	 * 
+	 * @return a new slide with an image or null if it hits a dead end without finding an image file.
+	 */
+	private Slide fetchRandomFile( File path ) {
 		ArrayList<File> fl = new ArrayList<File>( Arrays.asList( path.listFiles()));
 		
 		while( 0 < fl.size()) {
@@ -181,7 +231,10 @@ public class SlideShow extends JFrame {
 				if( f.isDirectory())
 					return fetchRandomFile( f );
 
-				if( f.isFile() && f.getName().toLowerCase().endsWith( ".lnk" )) {
+				/*
+				 * If the path is a link find the original path
+				 */
+				 if( f.isFile() && f.getName().toLowerCase().endsWith( ".lnk" )) {
 					fl.remove( i );
 					LnkParser lp = new LnkParser( f );
 
@@ -192,8 +245,20 @@ public class SlideShow extends JFrame {
 					}
 				}
 
+				/*
+				 * Load image from the file, make a new slide and return it.
+				 */
+				BufferedImage bi = ImageIO.read( f );
+
+				if( null == bi )
+					throw new IOException( f.getCanonicalPath() + " is not an image" );
+
 				System.out.println( f.getCanonicalPath());
-				return ImageIO.read( f );
+				Slide s = new Slide();
+				s.image = bi;
+				s.path = f.getCanonicalPath();
+
+				return s;
 			}
 			catch( IOException e ) {
 				fl.remove( i );
